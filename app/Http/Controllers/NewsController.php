08cb7fdd427b8e36761ddd2a893936b236480ff8
Igor Borisov <latentpickuper@gmail.com>
2020-03-27 14:39:10 +0300

    Форма

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -6,153 +6,156 @@ use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    private $news = [
-        1 => [
-            'id' => 1,
-            'category_id' => 1,
-            'title' => 'Новость 1',
-            'text_short' => 'Текст новости',
-            'text_full' => 'Полный текст новости'
-        ],
-        2 => [
-            'id' => 2,
-            'category_id' => 1,
-            'title' => 'Новость 2',
-            'text_short' => 'Текст новости',
-            'text_full' => 'Полный текст новости'
-        ],
-        3 => [
-            'id' => 3,
-            'category_id' => 1,
-            'title' => 'Новость 3',
-            'text_short' => 'Текст новости',
-            'text_full' => 'Полный текст новости'
-        ],
-        4 => [
-            'id' => 4,
-            'category_id' => 2,
-            'title' => 'Новость 4',
-            'text_short' => 'Текст новости',
-            'text_full' => 'Полный текст новости'
-        ],
-        5 =>
-            ['id' => 5,
-                'category_id' => 2,
-                'title' => 'Новость 5',
-                'text_short' => 'Текст новости',
-                'text_full' => 'Полный текст новости'
-            ],
-        6 =>
-            ['id' => 6,
-                'category_id' => 2,
-                'title' => 'Новость 6',
-                'text_short' => 'Текст новости',
-                'text_full' => 'Полный текст новости'
-            ],
-        7 =>
-            ['id' => 7,
-                'category_id' => 3,
-                'title' => 'Новость 7',
-                'text_short' => 'Текст новости',
-                'text_full' => 'Полный текст новости'
-            ],
-        8 =>
-            ['id' => 8,
-                'category_id' => 3,
-                'title' => 'Новость 8',
-                'text_short' => 'Текст новости',
-                'text_full' => 'Полный текст новости'
-            ],
-        9 =>
-            ['id' => 9,
-                'category_id' => 3,
-                'title' => 'Новость 9',
-                'text_short' => 'Текст новости',
-                'text_full' => 'Полный текст новости'
-            ],
-        10 =>
-            ['id' => 10,
-                'category_id' => 4,
-                'title' => 'Новость 10',
-                'text_short' => 'Текст новости',
-                'text_full' => 'Полный текст новости'
-            ],
-        11 =>
-            ['id' => 11,
-                'category_id' => 4,
-                'title' => 'Новость 11',
-                'text_short' => 'Текст новости',
-                'text_full' => 'Полный текст новости'
-            ],
-        12 =>
-            ['id' => 12,
-                'category_id' => 4,
-                'title' => 'Новость 12',
-                'text_short' => 'Текст новости',
-                'text_full' => 'Полный текст новости'
-            ],
+  private $news = [
+    1 => [
+      'id' => 1,
+      'category_id' => 1,
+      'title' => 'Новость 1',
+      'text_short' => 'Текст новости',
+      'text_full' => 'Полный текст новости'
+    ],
+    2 => [
+      'id' => 2,
+      'category_id' => 1,
+      'title' => 'Новость 2',
+      'text_short' => 'Текст новости',
+      'text_full' => 'Полный текст новости'
+    ],
+    3 => [
+      'id' => 3,
+      'category_id' => 1,
+      'title' => 'Новость 3',
+      'text_short' => 'Текст новости',
+      'text_full' => 'Полный текст новости'
+    ],
+    4 => [
+      'id' => 4,
+      'category_id' => 2,
+      'title' => 'Новость 4',
+      'text_short' => 'Текст новости',
+      'text_full' => 'Полный текст новости'
+    ],
+    5 =>
+      ['id' => 5,
+        'category_id' => 2,
+        'title' => 'Новость 5',
+        'text_short' => 'Текст новости',
+        'text_full' => 'Полный текст новости'
+      ],
+    6 =>
+      ['id' => 6,
+        'category_id' => 2,
+        'title' => 'Новость 6',
+        'text_short' => 'Текст новости',
+        'text_full' => 'Полный текст новости'
+      ],
+    7 =>
+      ['id' => 7,
+        'category_id' => 3,
+        'title' => 'Новость 7',
+        'text_short' => 'Текст новости',
+        'text_full' => 'Полный текст новости'
+      ],
+    8 =>
+      ['id' => 8,
+        'category_id' => 3,
+        'title' => 'Новость 8',
+        'text_short' => 'Текст новости',
+        'text_full' => 'Полный текст новости'
+      ],
+    9 =>
+      ['id' => 9,
+        'category_id' => 3,
+        'title' => 'Новость 9',
+        'text_short' => 'Текст новости',
+        'text_full' => 'Полный текст новости'
+      ],
+    10 =>
+      ['id' => 10,
+        'category_id' => 4,
+        'title' => 'Новость 10',
+        'text_short' => 'Текст новости',
+        'text_full' => 'Полный текст новости'
+      ],
+    11 =>
+      ['id' => 11,
+        'category_id' => 4,
+        'title' => 'Новость 11',
+        'text_short' => 'Текст новости',
+        'text_full' => 'Полный текст новости'
+      ],
+    12 =>
+      ['id' => 12,
+        'category_id' => 4,
+        'title' => 'Новость 12',
+        'text_short' => 'Текст новости',
+        'text_full' => 'Полный текст новости'
+      ],
 
-    ];
+  ];
 
-    private $categories = [
-        1 => 'Главные новости',
-        2 => 'Политика',
-        3 => 'Спорт',
-        4 => 'Культура'
-    ];
+  private $categories = [
+    1 => 'Главные новости',
+    2 => 'Политика',
+    3 => 'Спорт',
+    4 => 'Культура'
+  ];
 
-    public function index()
-    {
-        return redirect('/news/category/1');
+  public function index()
+  {
+    return redirect('/news/category/1');
+  }
+
+  public function category($id)
+  {
+    $categoryName = '';
+
+    if (!array_key_exists((int)$id, $this->categories)) {
+      abort('404');
     }
 
-    public function category($id)
-    {
-        $categoryName = '';
-
-        if (!array_key_exists((int)$id, $this->categories)) {
-            abort('404');
-        }
-
-        foreach ($this->categories as $key => $value) {
-            if ($key === (int)$id) {
-                $categoryName = $value;
-            }
-        }
-
-        $news = [];
-        foreach ($this->news as $key => $value) {
-            if ($value['category_id'] === (int)$id) {
-                $news[$key] = $value;
-            }
-        }
-
-        return view('news.category', [
-            'categoryName' => $categoryName,
-            'news' => $news
-        ]);
+    foreach ($this->categories as $key => $value) {
+      if ($key === (int)$id) {
+        $categoryName = $value;
+      }
     }
 
-    public function article($id)
-    {
-        if (!array_key_exists((int)$id, $this->news)) {
-            abort('404');
-        }
-
-        $article = null;
-
-        foreach ($this->news as $value) {
-            if ($value['id'] === (int)$id) {
-                $article = $value;
-            }
-        }
-
-        return view('news.article', ['article' => $article]);
-
+    $news = [];
+    foreach ($this->news as $key => $value) {
+      if ($value['category_id'] === (int)$id) {
+        $news[$key] = $value;
+      }
     }
 
-    public function articleCreate()
-    {
-        return view('news.create');
+    return view('news.category', [
+      'categoryName' => $categoryName,
+      'news' => $news
+    ]);
+  }
+
+  public function article($id)
+  {
+    if (!array_key_exists((int)$id, $this->news)) {
+      abort('404');
     }
+
+    $article = null;
+
+    foreach ($this->news as $value) {
+      if ($value['id'] === (int)$id) {
+        $article = $value;
+      }
+    }
+
+    return view('news.article', ['article' => $article]);
+
+  }
+
+  public function articleCreate(Request $request)
+  {
+    if ($request->isMethod('post')) {
+      return back()->with('success', 'Новость добавлена и ожидает модерации!');
+    }
+    return view('news.create');
+  }
 }
--- resources/views/news/create.blade.php
+++ resources/views/news/create.blade.php
@@ -10,26 +10,34 @@
     </div>
 
     <div class="container">
-        <form>
+        @if($message = Session::get('success'))
+            <div class="alert alert-success alert-block">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                <strong>{{ $message }}</strong>
+            </div>
+        @endif
+        <form method="post" action="{{route('news::create')}}">
+            @csrf
             <div class="form-group">
                 <label for="exampleFormControlSelect1">Категория</label>
-                <select class="form-control" id="exampleFormControlSelect1">
-                    <option>Политика</option>
-                    <option>Культура</option>
-                    <option>Спорт</option>
+                <select name="category" class="form-control" id="exampleFormControlSelect1">
+                    <option value="2">Политика</option>
+                    <option value="3">Культура</option>
+                    <option value="4">Спорт</option>
                 </select>
             </div>
             <div class="form-group">
                 <label for="exampleFormControlInput1">Заголовок</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Заголовок новости">
+                <input type="text" name="article_header" class="form-control" id="exampleFormControlInput1"
+                       placeholder="Заголовок новости">
             </div>
             <div class="form-group">
                 <label for="exampleFormControlTextarea1">Короткиий текст новости</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <textarea name="text_short" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
             <div class="form-group">
                 <label for="exampleFormControlTextarea1">Полный текст новости</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="7"></textarea>
+                <textarea name="text_full" class="form-control" id="exampleFormControlTextarea1" rows="7"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Отправить</button>
         </form>
